1944ca91a3c4665368808e23ea32c265ccb34b75
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-09 14:42:12 -0400

    Migrate files from toofz-necrodancer.

---
+++ toofz.NecroDancer.Leaderboards.PlayersService/PlayerSettings.cs
@@ -0,0 +1,9 @@
+﻿using toofz.NecroDancer.Leaderboards.Services.Common;
+
+namespace toofz.NecroDancer.Leaderboards.PlayersService
+{
+    sealed class PlayerSettings : Settings
+    {
+        public int PlayersPerUpdate { get; set; }
+    }
+}
