6fd01991ec60257aa25f87db2695927ffb6928d3
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-17 21:25:38 -0400

    v2.1.2

--- PlayersService/Properties/AssemblyInfo.cs
+++ PlayersService/Properties/AssemblyInfo.cs
@@ -2,13 +2,14 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+[assembly: AssemblyVersion("2.1.2.0")]
+
+[assembly: AssemblyCopyright("Copyright © Leonard Thieu 2017")]
+[assembly: AssemblyProduct("toofz")]
+
 [assembly: AssemblyTitle("toofz Players Service")]
-[assembly: AssemblyDescription("Provides Steam player data.")]
-[assembly: AssemblyCompany("toofz")]
-[assembly: AssemblyProduct("toofz Players Service")]
-[assembly: AssemblyCopyright("Copyright © Leonard Thieu 2015")]
+
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.1.1.*")]
 
 [assembly: InternalsVisibleTo("PlayersService.Tests")]
 
