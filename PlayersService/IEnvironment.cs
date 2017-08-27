c80802890c90b46ac3c7cb11710ec68f30d03b19
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-27 11:35:47 -0400

    Add tests for Program.

---
+++ PlayersService/IEnvironment.cs
@@ -0,0 +1,10 @@
+﻿namespace toofz.NecroDancer.Leaderboards.PlayersService
+{
+    interface IEnvironment
+    {
+        /// <summary>
+        /// Gets a value indicating whether the current process is running in user interactive mode.
+        /// </summary>
+        bool UserInteractive { get; }
+    }
+}
--- PlayersService/Program.cs
+++ PlayersService/Program.cs
@@ -22,29 +22,45 @@ namespace toofz.NecroDancer.Leaderboards.PlayersService
         /// </returns>
         static int Main(string[] args)
         {
-            Log.Debug("Initialized logging.");
-
-            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
             var settings = Settings.Default;
 
-            // Args are only allowed while running as a console as they may require user input.
-            if (Environment.UserInteractive && args.Any())
-            {
-                var parser = new PlayersArgsParser(Console.In, Console.Out, Console.Error, settings.KeyDerivationIterations);
+            return MainImpl(
+                args,
+                Log,
+                new EnvironmentAdapter(),
+                new PlayersArgsParser(Console.In, Console.Out, Console.Error, settings.KeyDerivationIterations),
+                settings,
+                new Application());
+        }
 
+        internal static int MainImpl(
+            string[] args,
+            ILog log,
+            IEnvironment environment,
+            IArgsParser<IPlayersSettings> parser,
+            IPlayersSettings settings,
+            IApplication application)
+        {
+            log.Debug("Initialized logging.");
+
+            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+
+            // Args are only allowed while running as a console as they may require user input.
+            if (args.Any() && environment.UserInteractive)
+            {
                 return parser.Parse(args, settings);
             }
 
             if (string.IsNullOrEmpty(settings.InstrumentationKey))
             {
-                Log.Warn($"The setting 'InstrumentationKey' is not set. Telemetry is disabled.");
+                log.Warn("The setting 'InstrumentationKey' is not set. Telemetry is disabled.");
             }
             else
             {
                 TelemetryConfiguration.Active.InstrumentationKey = settings.InstrumentationKey;
             }
 
-            Application.Run<WorkerRole, IPlayersSettings>();
+            application.Run<WorkerRole, IPlayersSettings>();
 
             return 0;
         }
