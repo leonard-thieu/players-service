3c28090149de13e4c12889e02327d0219cda19b7
Leonard Thieu <leonard.thieu@gmail.com>
2017-10-10 15:36:09 -0400

    oops

--- PlayersService.Tests/WorkerRoleTests.cs
+++ PlayersService.Tests/WorkerRoleTests.cs
@@ -94,22 +94,5 @@ namespace toofz.NecroDancer.Leaderboards.PlayersService.Tests
                 public Task PublicRunAsyncOverride(CancellationToken cancellationToken) => RunAsyncOverride(cancellationToken);
             }
         }
-
-        class StubPlayersSettings : IPlayersSettings
-        {
-            public int PlayersPerUpdate { get; set; }
-            public string ToofzApiBaseAddress { get; set; }
-            public string ToofzApiUserName { get; set; }
-            public EncryptedSecret ToofzApiPassword { get; set; }
-            public EncryptedSecret SteamWebApiKey { get; set; }
-            public TimeSpan UpdateInterval { get; set; }
-            public TimeSpan DelayBeforeGC { get; set; }
-            public string InstrumentationKey { get; set; }
-            public int KeyDerivationIterations { get; set; }
-
-            public void Reload() { }
-
-            public void Save() { }
-        }
     }
 }
