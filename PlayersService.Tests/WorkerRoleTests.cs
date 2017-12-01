8be4143859eb9b26c7293f1df1da115af888e5e7
Leonard Thieu <leonard.thieu@gmail.com>
2017-12-01 14:04:24 -0500

    Clean up tests.

--- PlayersService.Tests/WorkerRoleTests.cs
+++ PlayersService.Tests/WorkerRoleTests.cs
@@ -1,35 +1,20 @@
 ﻿using System;
-using System.Configuration;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using log4net;
 using Microsoft.ApplicationInsights;
 using Moq;
 using Ninject.Extensions.NamedScope;
-using toofz.NecroDancer.Leaderboards.PlayersService.Properties;
 using toofz.NecroDancer.Leaderboards.Steam.WebApi;
-using toofz.Services;
 using Xunit;
 
 namespace toofz.NecroDancer.Leaderboards.PlayersService.Tests
 {
     public class WorkerRoleTests
     {
-        public class IntegrationTests : DatabaseTestsBase
+        public class IntegrationTests : IntegrationTestsBase
         {
-            private readonly string settingsFileName = Path.GetTempFileName();
             private readonly Mock<ILog> mockLog = new Mock<ILog>();
-
-            protected override void Dispose(bool disposing)
-            {
-                if (disposing)
-                {
-                    if (File.Exists(settingsFileName)) { File.Delete(settingsFileName); }
-                }
-
-                base.Dispose(disposing);
-            }
 
             [Trait("Category", "Uses file system")]
             [Fact]
@@ -42,24 +27,19 @@ namespace toofz.NecroDancer.Leaderboards.PlayersService.Tests
                 }
                 db.SaveChanges();
 
-                var settings = Settings.Default;
-                // Should only loop once
-                foreach (SettingsProvider provider in settings.Providers)
-                {
-                    var ssp = (ServiceSettingsProvider)provider;
-                    ssp.GetSettingsReader = () => File.OpenText(settingsFileName);
-                    ssp.GetSettingsWriter = () => File.CreateText(settingsFileName);
-                }
                 settings.UpdateInterval = TimeSpan.Zero;
                 var telemetryClient = new TelemetryClient();
                 var runOnce = true;
 
                 var kernel = KernelConfig.CreateKernel();
 
-                var connectionString = StorageHelper.GetDatabaseConnectionString();
                 kernel.Rebind<string>()
-                      .ToConstant(connectionString)
+                      .ToConstant(databaseConnectionString)
                       .WhenInjectedInto(typeof(LeaderboardsContext), typeof(LeaderboardsStoreClient));
+
+                kernel.Rebind<ILeaderboardsContext>()
+                      .To<LeaderboardsContext>()
+                      .InParentScope();
 
                 kernel.Rebind<ILeaderboardsStoreClient>()
                       .To<LeaderboardsStoreClient>()
