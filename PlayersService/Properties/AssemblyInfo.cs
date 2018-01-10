c97d5dae1252a2799caa376dc4a6dc90178c0790
Leonard Thieu <leonard.thieu@gmail.com>
2018-01-10 07:59:59 -0500

    Update toofz Build to v2.4.0.

--- PlayersService/Properties/AssemblyInfo.cs
+++ PlayersService/Properties/AssemblyInfo.cs
@@ -1,16 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
 using log4net.Config;
-
-[assembly: AssemblyVersion("3.2.7.0")]
-
-[assembly: AssemblyCopyright("Copyright © Leonard Thieu 2017")]
-[assembly: AssemblyProduct("toofz")]
-
-[assembly: AssemblyTitle("toofz Players Service")]
-
-[assembly: ComVisible(false)]
 
 [assembly: InternalsVisibleTo("PlayersService.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
